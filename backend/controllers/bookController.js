ae9636c07e7342778f7e025da06853a7ca1816d7
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-07-11 16:26:28 -0400

    Delete book method

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,24 +1,33 @@
-const Book=require('../models/book')
-exports.getAllBook= async (req,res,next)=>{
-    const books= await Book.findAll();;
-   res.status(200).json(books)
+const Book = require('../models/book')
+exports.getAllBook = async (req, res, next) => {
+  const books = await Book.findAll();;
+  res.status(200).json(books)
 }
-exports.save= async (req,res,next)=>{
-  try{
-    const book=req.body;
-    const newBook= await new Book(null,book.title,book.ISBN,book.publishDate,book.author,book.price).save() 
-  //newBook is un object returned from the data base ops is property
-    res.status(201).json(newBook.ops); 
-  }catch(err){
-      next(err)
+exports.save = async (req, res, next) => {
+  try {
+    const book = req.body;
+    const newBook = await new Book(null, book.title, book.ISBN, book.publishDate, book.author, book.price).save()
+    //newBook is un object returned from the data base ops is property
+    res.status(201).json(newBook.ops);
+  } catch (err) {
+    next(err)
   }
- 
+
 }
-exports.getById= async (req,res,next)=>{
-  try{
-    const book=await Book.findById(req.params.id)
+exports.getById = async (req, res, next) => {
+  try {
+    const book = await Book.findById(req.params.id)
     res.status(200).json(book);
-  }catch(err){
-   next(err);
-  }    
+  } catch (err) {
+    next(err);
+  }
+
+}
+exports.delete = async (req, res, next) => {
+  try {
+    await Book.deleteById(req.params.id)
+    res.status(200).json({ deleted: "successfully" })
+  } catch (err) {
+    next(err)
+  }
 }
