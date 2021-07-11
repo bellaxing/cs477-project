bda40ead3aaae0dcec0f2415c91bc536fd4da587
haben2022 <berhanetesfahannes11@gmail.com>
2021-07-11 15:42:49 -0500

    cart routes

---
+++ backend/routes/cart.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const cartController=require('../controllers/cartController')
+router.get('/',cartController.getAllCart)
+router.post('/',cartController.save)
+router.delete('/remove/:username',cartController.delete)
+router.delete('/:id',cartController.delete)
+module.exports = router;
